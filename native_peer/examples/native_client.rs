82c08e7c4e7f9db546d0f64f04dc9ee6d6bc3c9b
Srayan Jana <valorzard.kiko@outlook.com>
2026-06-11 14:37:01 -0700

    fix lints native client

--- native_peer/examples/native_client.rs
+++ native_peer/examples/native_client.rs
@@ -1,7 +1,7 @@
-use anyhow::{Result, anyhow, bail};
+use anyhow::Result;
 use clap::Parser;
 use datachannel_socket_common::PeerFactory;
-use datachannel_socket_native_peer::{NativeClientPeerFactory, NativePeer, RoleAction};
+use datachannel_socket_native_peer::{NativeClientPeerFactory, RoleAction};
 use tokio::sync::oneshot;
 
 #[derive(Debug, Parser)]
